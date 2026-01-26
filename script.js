abba8c91b0726aeff3278364d11bf41a30d945f7
rahultripathi8795165-netizen <rahul.tripathi8795165@gmail.com>
2026-01-26 22:32:40 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const pullString = document.getElementById('pull-string');
-
-// When the user clicks the string
-pullString.addEventListener('click', () => {
-    // Add or remove the "on" class from the body
-    document.body.classList.toggle('on');
-});
